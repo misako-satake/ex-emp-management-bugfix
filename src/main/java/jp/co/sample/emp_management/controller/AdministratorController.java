7696455e1a18726436cf514213b4640f4989762f
misako-satake <misako.satake@rakus-partners.co.jp>
2021-11-16 12:00:59 +0900

    (1-2)演習　管理者登録完了後の遷移画面を修正しました。

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -84,7 +84,7 @@ public class AdministratorController {
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
 		administratorService.insert(administrator);
-		return "redirect:administrator/login";
+		return "redirect:/";
 	}
 
 	/////////////////////////////////////////////////////
