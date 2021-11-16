84b5c9f02003e52a15f8676567270f2c1bc4a5b6
misako-satake <misako.satake@rakus-partners.co.jp>
2021-11-16 11:50:15 +0900

    (1-2)演習　管理者登録の入力値チェック　AdministratorControllerのinsertメソッドを改修しました。

--- src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
@@ -1,5 +1,11 @@
 package jp.co.sample.emp_management.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,10 +14,15 @@ package jp.co.sample.emp_management.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@Length(min=1,max=20,message="名前は1文字以上20文字以下です")
 	private String name;
 	/** メールアドレス */
+	@NotBlank(message="メールアドレスを入力してください")
+	@Email(message="メールアドレスの形式が不正です")
 	private String mailAddress;
 	/** パスワード */
+	@Length(min=8,message="パスワードは8文字以上です")
+	@Pattern(regexp="^[0-9a-zA-Z]+$",message="パスワードは全半角英数字で入力してください")
 	private String password;
 
 	/**
